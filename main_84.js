47b1554ebcf9d8a50024479488a2d9cc3b3c0c21
MartinYacsavilca <126027562+MartinYacsavilca@users.noreply.github.com>
2024-04-30 17:49:41 -0500

    Add files via upload

---
+++ main_84.js
@@ -0,0 +1,10 @@
+pizza_list_array = [];
+
+function getmenu()
+	{    
+        var item = document.getElementById("nombre_pizza").value; 
+        pizza_list_array.push(item);
+        
+        var remove_commas = pizza_list_array.join("<br># ");
+        document.getElementById("mostrar_menu").innerHTML =  "# " + remove_commas;
+	}
